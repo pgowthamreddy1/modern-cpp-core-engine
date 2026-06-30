9a62225a3bb910bc603eba7f945a79398765e93a
P Gowtham Kumar Reddy <pgowthamreddy3@gmail.com>
2026-06-30 12:55:45 +0530

    Fix include directive in C++ program

--- DAY01_understanding the variables and syntax of the cpp program.cpp
+++ DAY01_understanding the variables and syntax of the cpp program.cpp
@@ -1,4 +1,4 @@
-include <iostream>
+#include <iostream>
   using namespace std;
 int main(){
 cout<<"hello world👋\n iam P.Gowtham Kumar Reddy😃\n iam a student of PES UNIVERSITY\niam learning c++ programming language before starting the collge\n i hope i will success in my life✌️\n";
