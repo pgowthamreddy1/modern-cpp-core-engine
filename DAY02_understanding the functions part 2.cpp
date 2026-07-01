a72a36c7ef9144212860493d7ff4e5e1521c28d5
P Gowtham Kumar Reddy <pgowthamreddy3@gmail.com>
2026-07-01 10:39:45 +0530

    Add basic C++ program to display score and grade

---
+++ DAY02_understanding the functions part 2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int score = 95;
+    char grade = 'A';
+    float percentage = 95.6;
+
+    cout << "Your score is: " << score << endl;
+    cout << "Your grade is: " << grade << endl;
+    cout <<"iam gowtham"<< endl;
+    
+    return 0; 
+}
