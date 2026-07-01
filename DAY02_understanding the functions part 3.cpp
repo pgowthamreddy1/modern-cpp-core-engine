cff22f2036d676440a11d0db4a5a8555a1fe0005
P Gowtham Kumar Reddy <pgowthamreddy3@gmail.com>
2026-07-01 10:42:10 +0530

    Add basic score and grade output in C++

---
+++ DAY02_understanding the functions part 3.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int score = 95;
+    char grade = 'A';
+    float percentage = 95.6; 
+
+    cout << "Your score is: " << score << endl; 
+    cout << "Your grade is: " << grade << endl;
+    
+    return 0; 
+}
